703b7b5ea7793223344206587105c920f899b2cc
nadimoh <62818730+nadimoh@users.noreply.github.com>
2020-03-29 13:13:26 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿kconst Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("692507870452908070")
+setInterval(function() {
+channel.send(`bost bots bots bots bots`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
